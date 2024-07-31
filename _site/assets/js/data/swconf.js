fd1ebe57ea6fdb64ec42f9d07f7762b4237622b3
JOONHA LEE <joonhalee@Joonha-MacBook-Air.local>
2024-07-31 20:45:26 +0900

    updated gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722425772',resources: [
+    cacheName: 'chirpy-1722426155',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
