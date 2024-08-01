e89f1c0e6b8892e429abff08d349aff45114033b
JOONHA LEE <joonhalee@Joonha-MacBook-Air.local>
2024-08-01 23:21:34 +0900

    poa post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722443009',resources: [
+    cacheName: 'chirpy-1722522055',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
